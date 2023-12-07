e91c36321b81862c5a3de36bd90bfab3401562dd
Yasmim Oliveira <139164469+yasmimfos@users.noreply.github.com>
2023-12-07 19:56:37 -0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::post("/users", [UserController::class, 'store']);
-Route::post('login', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get("/users", [UserController::class, 'index']);
